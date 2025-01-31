59a5ea36f06c38130e01e7c1d1c0018f102ebf77
luke <142288087+lukemawuntu@users.noreply.github.com>
2025-02-01 05:42:24 +0800

    Initial page styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log(1)
+import "./styles.css";
+import locationImg from './location.svg';
+import mailImg from './mail.svg';
+import phoneImg from './phone.svg';
+import overlayImg from './phone-overlay.png';
